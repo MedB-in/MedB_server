c298e2a5e5840d6b674eb325af2be7146bf43cdf
ThanoojKhan <thanu1495@gmail.com>
2025-02-04 17:48:20 +0530

    iniatial commit

--- src/config/postgresConnection.js
+++ src/config/postgresConnection.js
@@ -1,28 +1,37 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
 const env = require('../util/validateEnv');
 
 const db = env.POSTGRES_CONNECTION_STRING;
 
 const sequelize = new Sequelize(db, {
     dialect: 'postgres',
-    logging: false, // Disable query logging; set to true for debugging
+    logging: false,  // Disable query logging
+    dialectOptions: {
+        ssl: {
+            require: true,  
+            rejectUnauthorized: true,  
+            ca: fs.readFileSync(path.join(__dirname, '../../certs/ca.pem')).toString(),
+        },
+    },
 });
 
 const connectPostgreSQL = async (retries = 3) => {
     while (retries) {
         try {
             await sequelize.authenticate();
-            console.log('PostgreSQL database connected successfully');
+            console.log('✅ PostgreSQL database connected successfully');
             break;
         } catch (error) {
             retries -= 1;
             console.error(
-                `PostgreSQL connection failed. Retrying (${3 - retries}/3)...`,
+                `❌ PostgreSQL connection failed. Retrying (${3 - retries}/3)...`,
                 error.message
             );
             if (!retries) {
-                console.error('All retries failed. Skipping PostgreSQL connection.');
+                console.error('❌ All retries failed. Skipping PostgreSQL connection.');
                 throw error;
             }
             await new Promise((res) => setTimeout(res, 2000));
@@ -32,10 +41,10 @@ const connectPostgreSQL = async (retries = 3) => {
 
 (async () => {
     try {
-        await sequelize.sync({ force: false });
-        console.log('Database synchronized successfully.');
+        await sequelize.sync({ alter: true }); 
+        console.log('✅ Database synchronized successfully.');
     } catch (error) {
-        console.error('Error synchronizing database:', error.message);
+        console.error('❌ Error synchronizing database:', error.message);
     }
 })();
 
